9fbec620998f87d3458f773b20237397a97bdb77
Katie <katiem1013@hotmail.co.uk>
2026-01-31 22:40:14 +0000

    Made the menus look good

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,15 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Windows.Speech;
 
 public class Menu : MonoBehaviour
 {
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+    private void RecognisedSpeech(PhraseRecognizedEventArgs speech)
+    {
+        // listens for the speech and prints it in the console
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    public void ClearAudio()
+    {
+        // clears the old audio so it can be remade on a restart
+        if (keywordRecognizer != null && keywordRecognizer.IsRunning)
+        {
+            keywordRecognizer.OnPhraseRecognized -= RecognisedSpeech;
+            keywordRecognizer.Stop();
+            keywordRecognizer.Dispose();
+            // Crucial step to force a hard reset
+            PhraseRecognitionSystem.Shutdown();
+        }
+    }
+
+    private void Start()
+    {
+        // voice controls
+        actions.Add("level", LevelSelect);
+        actions.Add("quit", QuitGame);
+        actions.Add("exit", QuitGame);
+
+        actions.Add("level one", Level1);
+        actions.Add("one", Level1);
+
+        actions.Add("level two", Level2);
+        actions.Add("two", Level2);
+
+        actions.Add("level three", Level3);
+        actions.Add("three", Level3);
+
+        actions.Add("back", StartMenu);
+
+        // creates the words to listen
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognisedSpeech;
+        keywordRecognizer.Start();
+    }
+
     public GameObject startMenu;
     public GameObject levelSelectMenu;
 
     public void LevelSelect()
     {
-        startMenu.SetActive(false);
-        levelSelectMenu.SetActive(true);
+        if(startMenu.activeSelf){
+            startMenu.SetActive(false);
+            levelSelectMenu.SetActive(true);
+        }
     }
 
     public void StartMenu()
@@ -20,18 +73,34 @@ public class Menu : MonoBehaviour
     
     public void Level1()
     {
-        SceneManager.LoadScene(1);
+        if (levelSelectMenu.activeSelf)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
     public void Level2()
     {
-        SceneManager.LoadScene(2);
+        if (levelSelectMenu.activeSelf)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
+    public void Level3()
+    {
+        if (levelSelectMenu.activeSelf)
+        {
+            SceneManager.LoadScene(3);
+        }
+    }
 
     public void QuitGame()
     {
-        Application.Quit();
+        if (startMenu.activeSelf)
+        {
+            Application.Quit();
+        }
     }
 
     
